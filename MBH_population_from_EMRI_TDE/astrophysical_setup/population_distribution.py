366e76150cbf3db7f70b6c015416a826eeef4339
Shashwat Singh <shashwat98singh@gmail.com>
2026-03-20 17:05:02 +0000

    addition of distribution dN_da_dz

--- MBH_population_from_EMRI_TDE/astrophysical_setup/population_distribution.py
+++ MBH_population_from_EMRI_TDE/astrophysical_setup/population_distribution.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import math 
 
 from scipy.interpolate import RegularGridInterpolator
 
@@ -30,20 +31,36 @@ class Distribution2D:
 
         self.pdf_grid = None
         self.interp = None
+
+    def _sanitise_inputs(self, **inputs):
+        outs = {}
+        for a, b in inputs.items():
+            b_tensor = torch.as_tensor(b, device=self.device)
+            if b_tensor.ndim == 0:
+                b_tensor = b_tensor[None]
+            outs[a] = b_tensor
+        return outs
     
     def pdf(self):
         raise NotImplementedError
 
-    def interpolate(self, pdf_2d):
+    def evaluate_at_z_theta(self, z, theta):
+        """
+        Continuous PDF evaluation using 2D interpolation.
+        z, theta can be scalars or arrays.
+        """
+        pts = np.array([z, theta]).T  # shape (N, 2)
+        return self.interp(pts)
+
+    def interpolate(self, pdf_2d_normalized):
         """
         Build 2D interpolator using SciPy (continuous PDF evaluation)
         """
-        pdf_2d = pdf_2d / np.sum(pdf_2d)  # normalize
 
-        self.pdf_grid = pdf_2d
+        self.pdf_grid = pdf_2d_normalized
         self.interp = RegularGridInterpolator(
             (self.z_np, self.theta_np),
-            pdf_2d,
+            pdf_2d_normalized,
             bounds_error=False,
             fill_value=0.0
         )
@@ -136,23 +153,88 @@ class dN_dlgMBH_dz(Distribution2D):
                 T_obs = self.T_obs_det / (1 + z)
 
                 pdf[i, j] = dVc_dz * phi[j] * Gamma * T_obs
-                print(i, j, z, lgMBH, dVc_dz, phi[j], pdf[i, j])
-
+                # print(i, j, z, lgMBH, dVc_dz, phi[j], pdf[i, j])
+        
+        pdf /= np.sum(pdf)
         self.interpolate(pdf)
         return pdf
 
+class dN_da_dz(Distribution2D):
 
-dist = dN_dlgMBH_dz(limits_z=(0.001, 10), limits_theta=(4, 8.5), npoints=10)
-pdf = dist.pdf()
+    def __init__(self, limits_z, limits_theta, limits_MBH, npoints=200, grid_spacing='linear', device="cpu"):
+        super().__init__(limits_z, limits_theta, npoints, grid_spacing, device)
+        
+        self.dN_dlgMBH_dz = dN_dlgMBH_dz(limits_z, limits_MBH, npoints, grid_spacing, device)
+        self.a_grid = self.theta_grid 
 
-z_samp, mbh_samp = dist.draw_samples(200)
+        self.logMBH_grid = self.dN_dlgMBH_dz.theta_grid.cpu().numpy()
+        # self.logMBH_grid_max  = self.logMBH_grid.max()
 
-breakpoint()
+        self.dlogM = np.diff(self.logMBH_grid, prepend=self.logMBH_grid[0])
 
+    def pdf(self, **hypers):
+
+        Nz, Nm = len(self.z_grid), len(self.theta_grid)
+        pdf = np.zeros((Nz, Nm))
+
+        hypers = self._sanitise_inputs(**hypers)
+        beta = float(hypers["beta"])
+        lambda_alpha = float(hypers["lambda_alpha"])
+
+        lgalpha_M = ( beta + lambda_alpha * (self.logMBH_grid - 6) ) # shape (Nm,)
+
+        pdf_dN_dlgMBH_dz = self.dN_dlgMBH_dz.pdf()
+
+        for i, z in enumerate(self.z_grid.cpu().numpy()):
+
+            # pdf_mass[iz,:] is 1D array over M
+            fM = pdf_dN_dlgMBH_dz[i, :]                # shape (Nm,)
+
+            # compute beta PDF for all a values for each mass point
+            a = self.a_grid.cpu().numpy()       # shape (Na,)
+
+            # matrix of shape (Nm, Na)
+            beta_matrix = np.zeros((len(self.logMBH_grid), len(a)))
+
+            for j, lgalpha_j in enumerate(lgalpha_M):
+                # Beta(alpha_j, beta) normalization:
+
+                B = math.gamma(lgalpha_j) * math.gamma(beta) / math.gamma(lgalpha_j + beta)
+
+                beta_matrix[j,:] = (a**(lgalpha_j - 1)) * ((1-a)**(beta - 1)) / B
+
+                print(beta_matrix, B)
+            # Convolution in mass dimension:
+            # sum_j [ pdf_dN_dlgMBH_dz[z,j] * beta_pdf[j,a] * dlogM[j] ]
+            
+            pdf[i,:] = np.sum(fM[:,None] * beta_matrix * self.dlogM[:,None], axis=0)
+            # breakpoint()
+        pdf/=np.sum(pdf)
+        self.interpolate(pdf)
+        return pdf
+
+# dist_dN_dlgMBH_dz = dN_dlgMBH_dz(limits_z=(0.001, 10), limits_theta=(4, 8.5), npoints=10)
+# pdf_dN_dlgMBH_dz = dist.pdf()
+
+# z_samp, mbh_samp = dist.draw_samples(200)
+
+# plt.figure(figsize=(7,6))
+# plt.imshow(pdf.T, origin='lower',
+#            extent=[dist.z_np[0], dist.z_np[-1], dist.theta_np[0], dist.theta_np[-1]],
+#            aspect='auto', cmap='viridis')
+# plt.colorbar(label=r'$d^2N/d\log M\, dz$')
+# plt.xlabel('z')
+# plt.ylabel(r'$\log_{10} M_{\rm BH}$')
+# plt.title('EMRI 2D PDF')
+# plt.show()
+
+dist_dN_da_dz = dN_da_dz(limits_z=(0.001, 10), limits_theta=(0.1, 0.998), limits_MBH=(4, 8.5), npoints=5)
+pdf_dN_da_dz = dist_dN_da_dz.pdf(beta=6.0, lambda_alpha=2.7)
+z_samp, a_samp = dist_dN_da_dz.draw_samples(200)
 
 plt.figure(figsize=(7,6))
-plt.imshow(pdf.T, origin='lower',
-           extent=[dist.z_np[0], dist.z_np[-1], dist.theta_np[0], dist.theta_np[-1]],
+plt.imshow(pdf_dN_da_dz.T, origin='lower',
+           extent=[dist_dN_da_dz.z_np[0], dist_dN_da_dz.z_np[-1], dist_dN_da_dz.theta_np[0], dist_dN_da_dz.theta_np[-1]],
            aspect='auto', cmap='viridis')
 plt.colorbar(label=r'$d^2N/d\log M\, dz$')
 plt.xlabel('z')
